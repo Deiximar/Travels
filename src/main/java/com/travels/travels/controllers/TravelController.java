25e07ecb069cc8a7c5dd5e511b843d000436415c
Deiximar Virriel <deiximar@gmail.com>
2024-08-30 00:03:03 +0200

    Verify if user is authenticated and return travels

--- src/main/java/com/travels/travels/controllers/TravelController.java
+++ src/main/java/com/travels/travels/controllers/TravelController.java
@@ -1,9 +1,12 @@
 package com.travels.travels.controllers;
 
 import org.springframework.data.domain.Pageable;
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,7 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,22 +37,23 @@ public class TravelController {
   }
 
   @GetMapping("/travels")
-  public ResponseEntity<?> getTravels(@RequestParam(defaultValue = "0") int page,
-      @RequestParam(defaultValue = "8") int size, @RequestHeader(required = false) String token) {
+  public ResponseEntity<Page<Travel>> getTravels(@RequestParam(defaultValue = "0") int page,
+      @RequestParam(defaultValue = "8") int size) {
     Pageable pageable = PageRequest.of(page, size);
-    if (token != null && !token.isEmpty()) {
-      try {
-        int userId = userService.getUserId(token);
-        return ResponseEntity.ok(travelService.getTravels(userId, pageable));
-      } catch (UnauthorizedException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
-      } catch (UserNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    try {
+      Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+      if (auth instanceof AnonymousAuthenticationToken) {
+        return ResponseEntity.ok(travelService.getPublicTravels(pageable));
       }
-    } else {
-      return ResponseEntity.ok(travelService.getPublicTravels(pageable));
+      User user = (User) auth.getPrincipal();
+      return ResponseEntity.ok(travelService.getTravels(user.getId(), pageable));
+    } catch (UnauthorizedException e) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    } catch (UserNotFoundException e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
-  }
+  };
 
   @PostMapping("/travel")
   public ResponseEntity<Travel> addTravel(@RequestBody Travel travelRequest) {
